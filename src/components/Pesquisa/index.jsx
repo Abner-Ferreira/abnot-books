8f2fd948724f5b045ce4f820db3066355bfd7512
Abner-Ferreira <rm93576@fiap.com.br>
2023-10-12 22:57:09 -0300

    Projeto Finalizado

--- src/components/Pesquisa/index.jsx
+++ src/components/Pesquisa/index.jsx
@@ -1,29 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import Input from '../Input';
-import styled from 'styled-components';
+import Input from '../Input'
+import styled from 'styled-components'
+import { useState } from 'react'
 import { getLivros } from '../../services/livros'
+import { useEffect } from 'react'
+import { postFavorito } from '../../services/favoritos'
 
 const PesquisaContainer = styled.section`
-    background-img: linear-gradient(90deg, #002f52 35%, #326589 100%);
-    color: #fff;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+    color: #FFF;
     text-align: center;
     padding: 85px 0;
-    height: 270px;
-    width: 100%
-
+    height: 470px;
+    width: 100%;
 `
+
 const Titulo = styled.h2`
-    color: #fff;
+    color: #FFF;
     font-size: 36px;
     text-align: center;
-    width: 100%
-
+    width: 100%;
 `
+
 const Subtitulo = styled.h3`
     font-size: 16px;
     font-weight: 500;
     margin-bottom: 40px;
-
 `
 
 const Resultado = styled.div`
@@ -33,53 +34,57 @@ const Resultado = styled.div`
     margin-bottom: 20px;
     cursor: pointer;
 
-    p{
+    p {
         width: 200px;
     }
 
-    img{
+    img {
         width: 100px;
     }
 
-    &:hover{
-        border: 1px solid #fff
+    &:hover {
+        border: 1px solid white;
     }
 `
 
 function Pesquisa() {
-
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
-
     const [livros, setLivros] = useState([])
 
     useEffect(() => {
-        const livrosDaAPI = getLivros()
-        setLivros(livrosDaAPI)
+        fetchLivros()
     }, [])
 
-    return (
-        <>
-            <PesquisaContainer>
-                <Titulo>Já sabe por onde começar? </Titulo>
-                <Subtitulo>Econtre seu livro em nossa estante</Subtitulo>
-                <Input
-                    placeholder='Escreva sua próxima leitura'
-                    onBlur={evento => {
-                        const textoDigitado = evento.target.value
-                        const resultadoPesquisa = livros.filter(livro => livro.nome.includes(textoDigitado))
+    async function fetchLivros() {
+        const livrosDaAPI = await getLivros()
+        setLivros(livrosDaAPI)
+    }
 
-                        setLivrosPesquisados(resultadoPesquisa)
-                    }}
-                />
-                {livrosPesquisados.map((livro) => (
-                    <Resultado key={livro.id}>
-                        <p>{livro.nome}</p>
-                        <img src={livro.src} />
-                    </Resultado>
-                ))}
-            </PesquisaContainer>
-        </>
-    );
+    async function insereFavorito(id) {
+        await postFavorito(id)
+        alert(`O livro de id: ${id} foi inserido com sucesso`)
+    }
+
+    return (
+        <PesquisaContainer>
+            <Titulo>Já sabe por onde começar?</Titulo>
+            <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
+            <Input
+                placeholder="Escreva sua próxima leitura"
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado))
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}
+            />
+            { livrosPesquisados.map( livro => (
+                <Resultado onClick={() => insereFavorito(livro.id)}>
+                    <img src={livro.src}/>
+                    <p>{livro.nome}</p>
+                </Resultado>
+            ) ) }
+        </PesquisaContainer>
+    )
 }
 
-export default Pesquisa;
+export default Pesquisa
